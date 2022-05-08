f0ae4bee67b2d2e8f6b59a8345d083608d7f055f
Tianshuo Zhou <zhoutian@kruskal.eecs.oregonstate.edu>
2022-05-08 10:41:21 -0700

    node2vec

--- nc.py
+++ nc.py
@@ -6,9 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 
-import node2vec
+from node2vec import Node2Vec
 import networkx as nx
-
 
 def read_data(path):
     citef = path+".cites"
@@ -62,7 +61,7 @@ def cross_valid(features, labels, C_list=[0.1, 0.5, 1, 1.5, 2, 2.5, 5], iter_max
             c_best = C
             s_best = score_ave
             # lr_best = clf
-        print(f"C: {C:2.1f}, score_ave: {score_ave :.4f}")
+        print(f"C: {C:2.2f}, score_ave: {score_ave :.4f}")
     print(f"c_best: {c_best:2.1f}, s_best: {s_best :.4f}")
     return c_best
 
@@ -88,8 +87,12 @@ def main(args):
     print("class2label: ", class2label)
     labels = np.array([class2label[c] for c in nodes[:, -1]])
     
+    if args.count:
+        onehots = nodes[:, 1: -1].astype('float64')
+        print(f"onehots.shape: {onehots.shape}, labels.shape: {labels.shape}")
+        np.save(os.path.join(args.data_home, args.dataset, 'onehots.npy'), onehots)
     if args.words:
-        features = nodes[:, 1: -1].astype('float64')
+        features = np.load(os.path.join(args.data_home, args.dataset, 'onehots.npy'))
         print(f"features.shape: {features.shape}, labels.shape: {labels.shape}")
         learn(features, labels, C_list=args.c_list, iter_max=args.iter_max)
     if args.extract:
@@ -105,6 +108,21 @@ def main(args):
         feats_hstack = np.hstack((eigen_vectors[:, :args.truncate], features))
         print(f"feats_hstack.shape: {feats_hstack.shape}, labels.shape: {labels.shape}")
         learn(feats_hstack, labels, C_list=args.c_list, iter_max=args.iter_max)
+    if args.node2vec:
+        graph = nx.from_edgelist(edges)
+        nodes2vecs = Node2Vec(graph, dimensions=args.dimvec, walk_length=args.walk_length, num_walks=args.num_walks, workers=args.workers)
+        model = nodes2vecs.fit(window=5, min_count=1, batch_words=4)
+        EMBEDDING_FILENAME = os.path.join(args.data_home, args.dataset, f'nodes_{args.dimvec}.vec')
+        model.wv.save(EMBEDDING_FILENAME)
+        EMBEDDING_MODEL_FILENAME = os.path.join(args.data_home, args.dataset, f'node2vec_{args.dimvec}.model')
+        model.save(EMBEDDING_MODEL_FILENAME)
+    if args.vecs:
+        from gensim.models import KeyedVectors
+        EMBEDDING_FILENAME = os.path.join(args.data_home, args.dataset, f'nodes_{args.dimvec}.vec')
+        wv = KeyedVectors.load(EMBEDDING_FILENAME)
+        nodes2vecs = np.array([wv[node] for node in nodes_list])
+        print(f"nodes2vecs.shape: {nodes2vecs.shape}, labels.shape: {labels.shape}")
+        learn(nodes2vecs, labels, C_list=args.c_list, iter_max=args.iter_max)
     return
 
 
@@ -117,10 +135,16 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--words", action="store_true")
     parser.add_argument("--feats", action="store_true")
+    parser.add_argument("--vecs", action="store_true")
+    parser.add_argument("--count", action="store_true")
     parser.add_argument("--extract", action="store_true")
+    parser.add_argument("--node2vec", action="store_true")
     parser.add_argument("--truncate", type=int, default=512)
     parser.add_argument("--iter_max", type=int, default=500)
-    parser.add_argument('--nargs-int-type', nargs='+', type=int)
+    parser.add_argument("--dimvec", type=int, default=64)
+    parser.add_argument("--walk_length", type=int, default=30)
+    parser.add_argument("--num_walks", type=int, default=100)
+    parser.add_argument("--workers", type=int, default=8)
     parser.add_argument('-cl','--c_list', nargs='+', help='<Required> Set flag', type=float, default=[0.1, 1, 5, 10, 50, 100])
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--path_save", type=str, default='models')
